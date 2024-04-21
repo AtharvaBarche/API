e0906ef7c9e566ab36a6a7d386745f7f13356fe7
AtharvaBarche <barcheatharva01@gmail.com>
2024-04-21 20:14:31 +0530

    Removing Errors

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const PORT = 8000;
 const app=express();
 
 const userRoutes = require('./routes/User');
+
+app.use("/login",userRoutes);
 app.use("/",userRoutes);
+app.use("/profile:id",userRoutes);
+app.use("/profile:id",userRoutes);
+app.use("/register",userRoutes);
 
 app.listen(PORT,()=>{
     console.log(`App is running on $PORT`);
--- routes/User.js
+++ routes/User.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const controller = require("../controllers/UserController");
 
+router.post("/login",controller.login);
+router.post("/register",controller.register);
 router.get("/",controller.getUser);
 router.get("/profile:id", userController.getUserById);
-router.post("/login",controller.login);
-router.post("register",controller.register);
 router.delete("/profile:id", controller.deletePost);
 module.exports = router;
